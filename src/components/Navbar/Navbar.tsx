461de1d3071f2df6fce481f32fba84061553c109
MMansyy <hessenmansyy@gmail.com>
2025-07-22 03:28:22 +0300

    fix: update Navbar component for improved accessibility and styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -33,14 +33,14 @@ export default function Navbar() {
                         </ul>
                         <div className="flex items-center gap-3">
                             <Link to="/favourites" className="text-white hover:text-gray-300 transition-all duration-300">
-                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24"
+                                <svg stroke="currentColor" fill="none" strokeWidth="1" viewBox="0 0 24 24"
                                     className="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3c3.08 0 5.5 2.42 5.5 5.5 0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
                                 </svg>
                             </Link>
-                            <Link to="/search" className="text-white hover:text-gray-300 transition-all duration-300">
+                            <Link to="/search" className="">
                                 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg">
+                                    className="w-8 h-8 " xmlns="http://www.w3.org/2000/svg">
                                     <path d="M20.031,20.79c0.46,0.46,1.17-0.25,0.71-0.7l-3.75-3.76c1.27-1.41,2.04-3.27,2.04-5.31
                   c0-4.39-3.57-7.96-7.96-7.96s-7.96,3.57-7.96,7.96c0,4.39,3.57,7.96,7.96,7.96c1.98,0,3.81-0.73,5.21-1.94L20.031,20.79z
                   M4.11,11.02c0-3.84,3.13-6.96,6.96-6.96c3.84,0,6.96,3.12,6.96,6.96c0,3.84-3.12,6.96-6.96,6.96C7.24,17.98,4.11,14.86,4.11,11.02z">
@@ -72,10 +72,10 @@ export default function Navbar() {
                     <button onClick={() => setIsMenuOpen(false)} className="text-white text-2xl">✕</button>
                 </div>
                 <ul className="space-y-2 text-white text-xl">
-                    <li className=' bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg'><NavLink to="/">Home</NavLink></li>
-                    <li className=' bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg'><NavLink to="/movies">Movies</NavLink></li>
-                    <li className=' bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg'><NavLink to="/geners">Geners</NavLink></li>
-                    <li className=' bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg'><NavLink to="/tv-shows">TV Shows</NavLink></li>
+                    <NavLink to={'/'} onClick={() => setIsMenuOpen(false)} className=' '><li className='bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg' >Home</li></NavLink>
+                    <NavLink to={'/movies'} onClick={() => setIsMenuOpen(false)} className=' '><li className='bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg' >Movies</li></NavLink>
+                    <NavLink to={'/geners'} onClick={() => setIsMenuOpen(false)} className=' '><li className='bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg' >Geners</li></NavLink>
+                    <NavLink to={'/tv-shows'} onClick={() => setIsMenuOpen(false)} className=' '><li className='bg-transparent hover:bg-gray-800 transition-all duration-300  rounded-4xl py-2 px-4 text-lg' >TV Shows</li></NavLink>
                 </ul>
             </div>
         </>
